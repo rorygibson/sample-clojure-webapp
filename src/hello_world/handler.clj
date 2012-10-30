d08720110030387ede3dcc3d9c1e5a8f101c7803
Rory Gibson <rgibson@technophobia.com>
2012-10-30 15:26:16 +0000

    More refactoring

--- src/hello_world/handler.clj
+++ src/hello_world/handler.clj
@@ -4,77 +4,69 @@
             [compojure.handler :as handler]
             [compojure.route   :as route]))
 
-; Database creation and population
+;; Database creation and population
 (def db {:classname "com.mysql.jdbc.Driver"
          :subprotocol "mysql"
          :subname "//localhost:3306/helloworld"
          :user "root"
          :password ""})
 
-(defn drop-table-from
-  [conn]
+(defn drop-table-from [conn]
   (try
     (sql/with-connection conn
       (sql/drop-table :names))
     (catch Exception e (.getNextException e))))
 
-(defn create-table-from
-  [conn]
+(defn create-table-from [conn]
   (sql/with-connection conn
     (sql/create-table :names
-      [:first "varchar(32)" "PRIMARY KEY"]
-      [:second "varchar(32)"])))
+      [:forename "varchar(32)" "PRIMARY KEY"]
+      [:surname "varchar(32)"])))
 
-(defn insert-records-in
-  [conn]
+(defn insert-records-in [conn]
   (sql/with-connection conn
     (sql/insert-records :names
-      {:first "Rory" :second "Gibson"}
-      {:first "Bilbo" :second "Baggins"}
-      {:first "Martin" :second "Fowler"}
-      {:first "James" :second "Dean Bradley"}
-      {:first "Mick" :second "Jagger"})))
+      {:forename "Rory" :surname "Gibson"} 
+      {:forename "Bilbo" :surname "Baggins"} 
+      {:forename "Martin" :surname "Fowler"}
+      {:forename "James" :surname "Dean Bradley"} 
+      {:forename "Mick" :surname "Jagger"})))
 
-; Getting stuff from the DB
-(defn all-names-from
-  [conn]
+;; Getting stuff from the DB
+(defn all-names-from [conn]
     (sql/with-connection conn
       (sql/with-query-results rows
-        ["select first, second from names"]
+        ["select forename, surname from names"]
         (doall rows))))     
 
-(defn lookup-surname-of 
-  [firstname conn]  
+(defn lookup-surname-of [forename conn]  
     (sql/with-connection conn
       (sql/with-query-results rows
-        ["select second from names where first=?" firstname]
-        (:second (first rows)))))
+        ["select surname from names where forename=?" forename]
+        (:surname (first rows)))))
 
-; Generating HTML
-(defn li-for 
-  [name-map]
-  (str "<li> <a href=\"/" (:first name-map) "\">" (:first name-map) " " (:second name-map) "</a></li>"))   
+;; Generating HTML
+(defn li-for [name-map]
+  (str "<li> <a href=\"/" (:forename name-map) "\">" (:forename name-map) " " (:surname name-map) "</a></li>"))   
 
-(defn ul-of 
-  [seq-of-name-maps]
+(defn ul-of [seq-of-name-maps]
   (str "<ul>" (apply str (map li-for seq-of-name-maps)) "</ul>"))
 
-(defn hello
-  [fname sname]
+(defn hello [fname sname]
   (if (nil? sname)
-            (str "Hello " fname ", have we met before?")
-            (str "Hello " fname " " sname ", nice to see you again")))
+    (str "Hello " fname ", have we met before?")
+    (str "Hello " fname " " sname ", nice to see you again")))
 
-; Actually set up the DB
+;; Actually set up the DB
 (drop-table-from db)
 (create-table-from db)
 (insert-records-in db)
 
-; Compojure route/handler bindings
+;; Compojure route/handler bindings
 (defroutes app-routes
-  (GET "/:firstname" [firstname]
-    (let [surname (lookup-surname-of firstname db)]
-      (hello firstname surname)))          
+  (GET "/:forename" [forename]
+    (let [surname (lookup-surname-of forename db)]
+      (hello forename surname)))          
 
   (route/not-found 
     (let [heading "<h1>Uh oh</h1>"
